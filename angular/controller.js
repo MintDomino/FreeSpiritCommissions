f34089e87d05c69768a294f09cb5ed78c755746f
CasalettoJ <jake.m.casaletto@gmail.com>
2015-06-30 23:42:17 -0400

    styling

--- angular/controller.js
+++ angular/controller.js
@@ -81,14 +81,14 @@
       title: "Extra Characters",
       options: [
         {
-          title: "+ 1",
+          title: "+ 1 Character",
           modifier: 70,
           selected: false,
           index: 0,
           code: "~1"
         },
         {
-          title: "+ 2",
+          title: "+ 2 Character",
           modifier: 150,
           selected: false,
           index: 1,
@@ -125,11 +125,15 @@
 
     vm.total = 0.00;
     vm.code = "";
+    vm.checkingCode = "";
+    vm.cartOrder = [];
+    vm.cartModifier = [];
     vm.getTotal = getTotal;
     vm.resetColorOptions = resetColorOptions;
     vm.clearSketch = clearSketch;
     vm.clearInk = clearInk;
     vm.optionChange = optionChange;
+    vm.setCode = setCode;
 
 
     //FUNCTIONS
@@ -137,27 +141,34 @@
     function getTotal(){
       vm.total = 0.00;
       vm.code = "";
+      vm.cartOrder = [];
+      vm.cartModifier = [];
       //Get the tier
       if(vm.tiers.sketch.selected){
         vm.total += vm.tiers.sketch.price;
         vm.code += vm.tiers.sketch.code;
+        vm.cartOrder.push({title: vm.tiers.sketch.title, price: vm.tiers.sketch.price});
         if(vm.colorOptions.sketch.selected){
           vm.total += vm.colorOptions.sketch.price;
           vm.code += vm.colorOptions.sketch.code;
+          vm.cartOrder.push({title: vm.colorOptions.sketch.title, price: vm.colorOptions.sketch.price});
           vm.colorOptions.sketch.options.forEach(function(element){
             if(element.selected){
               vm.total += element.price;
               vm.code += element.code;
+              vm.cartOrder.push({title: element.title, price: element.price});
             }
           });
         }
         if(vm.nsfwChoices.selected){
           vm.total += vm.nsfwChoices.price;
           vm.code += vm.nsfwChoices.code;
+          vm.cartOrder.push({title: vm.nsfwChoices.title, price: vm.nsfwChoices.price});
           vm.nsfwChoices.options.forEach(function(element){
             if(element.selected){
               vm.total += element.price;
               vm.code += element.code;
+              vm.cartOrder.push({title: element.title, price: element.price});
             }
           });
         }
@@ -165,23 +176,28 @@
       else if(vm.tiers.ink.selected){
         vm.total += vm.tiers.ink.price;
         vm.code += vm.tiers.ink.code;
+        vm.cartOrder.push({title: vm.tiers.ink.title, price: vm.tiers.ink.price});
         if(vm.colorOptions.ink.selected){
           vm.total += vm.colorOptions.ink.price;
           vm.code += vm.colorOptions.ink.code;
+          vm.cartOrder.push({title: vm.colorOptions.ink.title, price: vm.colorOptions.ink.price});
           vm.colorOptions.ink.options.forEach(function(element){
             if(element.selected){
               vm.total += element.price;
               vm.code += element.code;
+              vm.cartOrder.push({title: element.title, price: element.price});
             }
           });
         }
         if(vm.nsfwChoices.selected){
           vm.total += vm.nsfwChoices.price;
           vm.code += vm.nsfwChoices.code;
+          vm.cartOrder.push({title: vm.nsfwChoices.title, price: vm.nsfwChoices.price});
           vm.nsfwChoices.options.forEach(function(element){
             if(element.selected){
               vm.total += element.price;
               vm.code += element.code;
+              vm.cartOrder.push({title: element.title, price: element.price});
             }
           });
         }
@@ -194,8 +210,49 @@
         if(vm.characterChoices.options[i].selected){
             vm.total += (vm.total * (vm.characterChoices.options[i].modifier)/100);
             vm.code += vm.characterChoices.options[i].code;
+            vm.cartModifier.push({title: vm.characterChoices.options[i].title, modifier: vm.characterChoices.options[i].modifier});
         }
       }
+    }
+
+    function setCode(){
+      //Set tiers
+      if(vm.checkingCode.indexOf(vm.tiers.sketch.code) > -1){
+        vm.tiers.sketch.selected = true;
+        if(vm.checkingCode.indexOf(vm.colorOptions.sketch.code) > -1){
+          vm.colorOptions.sketch.selected = true;
+          vm.colorOptions.sketch.options.forEach(function(element){
+            if(vm.checkingCode.indexOf(element.code) > -1){
+              element.selected = true;
+            }
+          });
+        }
+      }
+      if(vm.checkingCode.indexOf(vm.tiers.ink.code) > -1){
+        vm.tiers.ink.selected = true;
+        if(vm.checkingCode.indexOf(vm.colorOptions.ink.code) > -1){
+          vm.colorOptions.ink.selected = true;
+          vm.colorOptions.ink.options.forEach(function(element){
+            if(vm.checkingCode.indexOf(element.code) > -1){
+              element.selected = true;
+            }
+          });
+        }
+      }
+      if(vm.checkingCode.indexOf(vm.nsfwChoices.code) > -1){
+        vm.nsfwChoices.selected = true;
+        vm.nsfwChoices.options.forEach(function(element){
+          if(vm.checkingCode.indexOf(element.code) > -1){
+            element.selected = true;
+          }
+        });
+      }
+      vm.characterChoices.options.forEach(function(element){
+        if(vm.checkingCode.indexOf(element.code) > -1){
+          element.selected = true;
+        }
+      });
+      getTotal();
     }
 
     function resetItems(){
